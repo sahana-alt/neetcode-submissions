432246b2a70e032431b59de9fe3ae3743d29a687
sahana-alt <51981952+sahana-alt@users.noreply.github.com>
2026-07-03 00:13:35 -0400

    Add: two-integer-sum-ii - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-8.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+
+        int n = numbers.length;
+
+        for(int i=0;i<n;i++){
+
+            for(int j=i+1;j<n;j++){
+                
+                if(i!=j){
+                    int sum = numbers[i]+numbers[j];
+                    if(sum == target){
+                        return new int[]{Math.min(i+1,j+1),Math.max(i+1,j+1)};
+                    }
+                }
+            }
+        }
+
+        return new int[]{};
+
+
+        
+    }
+}
