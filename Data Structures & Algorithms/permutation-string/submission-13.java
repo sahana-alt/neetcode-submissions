ac4ed86bd77202e95cfc300e39a9dbeb4d8848d9
sahana-alt <51981952+sahana-alt@users.noreply.github.com>
2026-07-04 17:29:29 -0400

    Add: permutation-string - submission-13

---
+++ Data Structures & Algorithms/permutation-string/submission-13.java
@@ -0,0 +1,150 @@
+                class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        // int[] s1Count = new int[26];
+        // int[] s2Count = new int[26];
+
+        // for(int i=0;i<s1.length();i++){
+        //     s1Count[s1.charAt(i)-'a']++;
+        //     s2Count[s2.charAt(i)-'a']++;
+        // }
+        // int match =0; //the first window check
+
+        // for(int i=0;i<26;i++){
+        //     if(s1Count[i]==s2Count[i]){
+        //         match++;
+        //     }
+        // }
+
+        // int l=0; 
+        // for(int r=s1.length();r<s2.length();r++){
+        //     if(match==26){
+        //         return true;
+        //     }
+        //     int index= s2.charAt(r)-'a';
+        //     s2Count[index]++;
+
+        //     if(s1Count[index]==s2Count[index]){
+        //         match++;
+        //     } else if((s1Count[index]+1==s2Count[index])){
+        //         match--;
+        //     }
+
+        //     index= s2.charAt(l)-'a';
+        //     s2Count[index]--;
+        //     if(s1Count[index]==s2Count[index]){
+        //         match++;
+        //     } else if((s1Count[index]-1==s2Count[index])){
+        //         match--;
+        //     }
+        //     l++;
+        // }
+
+
+        // return false;
+
+        // int n = s1.length();
+        // int m = s2.length();
+
+        // char[] chars1 = s1.toCharArray();
+        // Arrays.sort(chars1);
+        // String s11 = new String(chars1);
+
+        // if(m<n){
+        //     return false;
+        // }
+
+        // for(int i = 0;i<=m-n;i++){
+
+            
+
+                
+        //         String sub = s2.substring(i,i+n);
+
+        //         char[] chars2 = sub.toCharArray();
+        //         Arrays.sort(chars2);
+        //         String s22 = new String(chars2);
+
+
+        //         if(s11.equals(s22)){
+        //             return true;
+
+        //         }
+
+            
+
+
+        // }
+
+        // return false;
+
+
+        // int n = s1.length();
+        // int m = s2.length();
+
+
+        // int[] s1Count = new int[26];
+        // int[] s2Count = new int[26];
+
+
+        // for(int i=0;i<n;i++){
+        //     s1Count[s1.charAt(i)-'a']++;
+        // }
+
+        // for(int i=0;i<=m-n;i++){
+
+            
+
+        //     int[] s2Count = new int[26];
+
+        //     String sub = s2.substring(i,i+n);
+        //     System.out.println(sub);
+
+        //     for(int j=0;j<n;j++){
+        //         s2Count[sub.charAt(j)-'a']++;  
+        //     }
+
+        //     if(Arrays.equals(s1Count,s2Count)){
+        //         return true;
+                
+        //     }
+        // }
+
+        // return false;
+
+
+
+        int n = s1.length();
+        int m = s2.length();
+
+
+        int[] s1Count = new int[26];
+        int[] s2Count = new int[26];
+
+
+        for(int i=0;i<n;i++){
+            s1Count[s1.charAt(i)-'a']++;
+        }
+
+        int l = 0, r=0;
+
+        while(r<m){
+            s2Count[s2.charAt(r)-'a']++;
+
+            if(r-l+1 > n){
+                s2Count[s2.charAt(l)-'a']--;
+                l++;
+            }
+
+            if(r-l+1 == n){
+                if(Arrays.equals(s1Count,s2Count)){
+                return true;      
+            }
+
+            }
+            r++;
+        }
+
+        return false;
+
+    }
+}
