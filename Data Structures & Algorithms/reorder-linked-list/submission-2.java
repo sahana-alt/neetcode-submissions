11155eba850db6758e5072e29fae92bff3889a55
sahana-alt <51981952+sahana-alt@users.noreply.github.com>
2026-07-07 16:46:51 -0400

    Add: reorder-linked-list - submission-2

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-2.java
@@ -0,0 +1,122 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public void reorderList(ListNode head) {
+    //     ListNode fast= head.next,slow=head, list1=head;
+    //     //problem on slow fast pointer and 
+    //     //reversing & merging list
+
+    //     while(slow!=null && fast!=null&& fast.next!=null){
+    //         slow = slow.next;
+    //         fast = fast.next.next;
+    //     }
+
+    //     ListNode list2 = slow.next,prev = null, temp;
+    //     slow.next = null;
+    //     while(list2!=null){
+    //         //reversing logic
+    //         temp = list2.next;
+    //         list2.next = prev;
+    //         prev = list2;
+    //         list2 = temp;
+    //     }
+    //     // so prev will be the head of 
+    //     //the head of the reversed list
+
+    //     ListNode dummy = new ListNode(-1);
+    //     ListNode cur = dummy; // imp:- cur and 
+    //     //dummy ppoint to same node
+        
+
+    // while(list1 !=null  && prev!=null){
+    //     //imp:- prev and l1 null check
+    //     // merging 2 lists
+    //     //imp:- u return void so u should 
+    //     //do inpact merging
+
+    //      ListNode temp1 = list1.next;
+    // ListNode temp2 = prev.next;
+
+    // list1.next = prev;
+    // if (temp1 == null) break;
+    // prev.next = temp1;
+
+    // list1 = temp1;
+    // prev = temp2;
+
+
+
+    // }
+
+    if(head == null || head.next == null){
+        return;
+    }
+
+    ListNode slow = head, fast= head.next;
+
+//find mid point
+    while(slow!=null && fast!=null && fast.next!=null){
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    //reverse the second half and store in a list
+
+    ListNode prev = null;
+    ListNode cur2 = slow.next;
+    slow.next = null; 
+
+    while(cur2!=null){
+        ListNode temp = cur2.next;
+        cur2.next = prev;
+        prev = cur2;
+        cur2 = temp;
+       
+    }
+
+   
+
+
+ 
+
+    ListNode newList = head;
+    ListNode curnewList = newList;
+    head= curnewList;
+    newList = newList.next;
+
+   
+  
+    System.out.println(curnewList.val+"-->");
+
+    while(newList!=null && prev!=null){
+         curnewList.next = prev;
+         prev= prev.next;
+        curnewList = curnewList.next;
+      
+        
+
+        curnewList.next = newList;
+        newList= newList.next;
+        curnewList = curnewList.next;
+  
+    }
+    curnewList.next = prev;
+
+
+
+   
+
+ 
+
+  
+    }
+}
