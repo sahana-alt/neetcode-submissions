e9da19bae9ee909235d2a91ba14d16b1c211ace5
sahana-alt <51981952+sahana-alt@users.noreply.github.com>
2026-07-13 02:05:01 -0400

    Add: clone-graph - submission-2

---
+++ Data Structures & Algorithms/clone-graph/submission-2.java
@@ -0,0 +1,80 @@
+/*
+Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        Map<Node,Node> mp = new HashMap<>();
+        // int i=1;
+        // if(node==null){
+        //     return null;
+        // }
+
+        // return dfs(node,mp);
+        return bfs(node,mp);
+        
+    }
+
+    public Node bfs(Node node, Map<Node,Node> mp){
+        if(node == null){
+            return null;
+        }
+        Queue<Node> q = new ArrayDeque<>();
+        Node copy = new Node(node.val);
+        mp.put(node,copy);
+        q.add(node);
+
+        while(!q.isEmpty()){
+            Node orig = q.poll();
+
+            for (Node neighbor : orig.neighbors) {
+            if (!mp.containsKey(neighbor)) {
+                mp.put(neighbor, new Node(neighbor.val));
+                q.add(neighbor);
+            }
+            mp.get(orig).neighbors.add(mp.get(neighbor));
+            }
+                     
+        }
+
+        return mp.get(node);
+    }
+
+    // public Node dfs(Node node,Map<Integer,Node> mp){
+    //     if(node==null){
+    //         return null;
+    //     }
+
+    //     if(mp.containsKey(node.val)) return mp.get(node.val);
+
+    //     Node copyNode = new Node(node.val);
+    //     mp.put(node.val, copyNode);
+
+    //     //dfs to neighnor nodes of originial and add to copy node
+
+    //     for(Node neighbor: node.neighbors){
+    //         copyNode.neighbors.add(dfs(neighbor,mp));
+    //     }
+
+    //     return copyNode;
+        
+    // }
+
+
+}
