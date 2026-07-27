542dcf3760baf9da029c9c4f21183a5da82a3d68
sahana-alt <51981952+sahana-alt@users.noreply.github.com>
2026-07-27 02:04:00 -0400

    Add: word-break - submission-2

---
+++ Data Structures & Algorithms/word-break/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+
+        int n = s.length();
+
+        boolean[] dp = new boolean[n+1];
+
+        dp[0] = true;
+
+        for(int i=1;i<=n;i++){
+            for(int j =0;j<=i;j++){
+                // System.out.println(s.substring(j,i));
+                
+                if(dp[j] && wordDict.contains(s.substring(j,i))){
+                    
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+        
+    }
+}
